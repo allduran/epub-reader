3bbff81f4c3e38ee515efeed9e41abe34fce8300
duran <cheshire.dd96@gmail.com>
2023-11-23 00:33:19 +0100

    updated everything

--- frontend/app.js
+++ frontend/app.js
@@ -1,72 +1,38 @@
-// Import necessary modules
-const Book = require("./book");
-const Rendition = require("./rendition");
-const CFI = require("./epubcfi");
-const utils = require("./utils/core");
+const fileInput = document.getElementById('fileInput');
+const viewer = document.getElementById('viewer');
+const loadButton = document.getElementById('loadButton'); // New button reference
+let currentBook;
 
-/**
- * Read and render an EPUB file
- * @param {File} file - EPUB file to be rendered
- */
-function readEPUB(file) {
-    // Get the viewer element
-    const viewer = document.getElementById('viewer');
+loadButton.addEventListener('click', handleFile); // Listen for button click
 
-    // Create a new Book instance with the uploaded EPUB file
-    const book = new Book(file);
+function handleFile() {
+    const file = fileInput.files[0];
 
-    // Set up a rendition to render the book to the viewer element
-    const rendition = new Rendition(book, {
-        flow: "auto", // You can adjust the flow as needed
-        width: "100%", // Set the width of the viewer
-        height: "100%" // Set the height of the viewer
-    });
-
-    // Display the book
-    rendition.display();
-
-    // Event listener for page changes
-    rendition.on('relocated', function (location) {
-        // Save the current page to localStorage or perform other actions
-        localStorage.setItem('currentPage', location.start.cfi);
-    });
-
-    // Load the last read page if available
-    const lastPage = localStorage.getItem('currentPage');
-    if (lastPage) {
-        rendition.display(lastPage);
-    }
-}
-
-// Event listener for file input change
-document.getElementById('fileInput').addEventListener('change', function () {
-    readEPUB(this.files[0]);
-});
-
-// Your existing functions and code can go below this point
-// ...
-
-// Example: Handle PDF files (you can adapt this for other file types)
-function readPDF(file) {
-    // Handle PDF file reading logic here
-    console.log('Reading PDF:', file.name);
-}
-
-// Example: Attach event listener for PDF files
-document.getElementById('fileInput').addEventListener('change', function () {
-    const file = this.files[0];
-    const fileType = getFileType(file.name);
-
-    if (fileType === 'epub') {
-        readEPUB(file);
-    } else if (fileType === 'pdf') {
-        readPDF(file);
+    if (file && file.name.endsWith('.epub')) {
+        loadEPUB(file);
     } else {
-        console.error('Unsupported file type.');
+        alert('Please select a valid EPUB file.');
     }
-});
+}
 
-// Helper function to get file type based on file extension
-function getFileType(fileName) {
-    return fileName.split('.').pop().toLowerCase();
+function loadEPUB(file) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        const arrayBuffer = event.target.result;
+        renderEPUB(arrayBuffer);
+    };
+
+    reader.readAsArrayBuffer(file);
+}
+
+function renderEPUB(arrayBuffer) {
+    ePub(arrayBuffer).then(book => {
+        currentBook = book;
+
+        const rendition = book.renderTo(viewer, { width: '100%', height: '100%' });
+        rendition.display();
+    }).catch(error => {
+        console.error('Error loading EPUB:', error);
+    });
 }
